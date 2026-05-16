83528a65e16c1f4adeaff0b5aa033e389b5cde25
Bernard Edet Ekoli <bernardedetekoli@gmail.com>
2026-05-16 13:52:26 +0100

    Updated Firebase.ts to point to env instead of json

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,9 +1,13 @@
+import dotenv from "dotenv"
+dotenv.config()
 import admin from "firebase-admin"
-import type { ServiceAccount } from "firebase-admin"
-import serviceAccount from "./ekmarkServiceAccountKey.json" with { type: "json" }
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as ServiceAccount)
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID!,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+    })
 })
 
 export const db = admin.firestore()
